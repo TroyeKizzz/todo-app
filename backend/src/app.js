bf409b88f6a05457fc13c68a2dc3e0e683100993
Sviatoslav Vasev <sviatoslav.vasev@tuni.fi>
2021-12-21 01:00:47 +0200

    Add more tasks

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,8 @@ require('dotenv').config();
 
 const middlewares = require('./middlewares');
 const api = require('./api');
+const { fake } = require('faker');
+faker.seed(123);
 
 const app = express();
 
@@ -24,19 +26,35 @@ app.get('/', (req, res) => {
 
 app.get('/task/random', (req, res) => {
   output = "";
-  //switch(Math.floor(Math.random() * 10)) {
-  switch(0) {
+  random_number = Math.floor(Math.random() * 7);
+  switch(random_number) {
     case 0:
-      // Buy {product} from {company_name}
-      const product = faker.commerce.productName();
-      const company_name = faker.commerce.companyName();
-      output = `Buy ${product} from ${company_name}`;
+      // Buy {product} from {company}
+      output = `Buy ${faker.commerce.productName()} from ${faker.company.companyName()}`;
       break;
     case 1:
       // Meet {name}
+      output = `Meet ${faker.name.firstName()}`;
+      break;
+    case 2:
+      // Call {company} at {phone_number}
+      output = `Call ${faker.company.companyName()} at ${faker.phone.phoneNumberFormat()}`;
+      break;
+    case 3:
+      // Send an email to {name} at {email}
+      output = `Send an email to ${faker.name.firstName()} at ${faker.internet.email()}`;
+      break;
+    case 4:
+      // Send {amount} {currency} to {name} at {iban}
+      output = `Send ${faker.finance.amount()} ${faker.finance.currencyCode()} to ${faker.name.firstName()} at ${faker.finance.iban()}`;
+      break;
+    case 5:
+      // Buy tickets for a trip to {location} in {month}
+      output = `Buy tickets for a trip to ${faker.address.city()} in ${faker.date.month()}`;
       break;
     default:
-      // code block
+      // Pick up {first_name} {last_name} from {address}
+      output = `Pick up ${faker.name.firstName()} ${faker.name.lastName()} from ${faker.address.streetAddress()}`;
   }
   res.json({
     message: output
